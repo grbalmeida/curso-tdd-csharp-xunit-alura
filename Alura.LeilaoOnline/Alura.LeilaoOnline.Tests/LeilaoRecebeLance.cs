b4058604e1509559b2e86e962a77238485b6ad13
gilvan <g.r.almeida@live.com>
2020-10-14 21:51:50 -0300

    Nomenclatura

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoRecebeLance.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using Alura.LeilaoOnline.Core;
+using System.Linq;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoRecebeLance
+    {
+        [Theory]
+        [InlineData(4, new double[] { 1000, 1200, 1400, 1300 })]
+        [InlineData(2, new double[] { 800, 900 })]
+        public void NaoPermiteNovosLancesDadoLeilaoFinalizado(
+            int qtdeEsperada, double[] ofertas)
+        {
+            // Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+
+            leilao.TerminaPregao();
+
+            // Act
+            leilao.RecebeLance(fulano, 1000);
+
+            // Assert
+            var qtdeObtida = leilao.Lances.Count();
+
+            Assert.Equal(qtdeEsperada, qtdeObtida);
+        }
+    }
+}
--- Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -3,13 +3,14 @@ using Xunit;
 
 namespace Alura.LeilaoOnline.Tests
 {
-    public class LeilaoTestes
+    public class LeilaoTerminaPregao
     {
         [Theory]
         [InlineData(1200, new double[] { 800, 900, 1000, 1200 })]
         [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
         [InlineData(800, new double[] { 800 })]
-        public void LeilaoComVariosLances(double valorEsperado, double[] ofertas)
+        public void RetornaMaiorValorDadoLeilaoComPeloMenosUmLance(
+            double valorEsperado, double[] ofertas)
         {
             // Arrange - cenário
             var leilao = new Leilao("Van Gogh");
@@ -30,7 +31,7 @@ namespace Alura.LeilaoOnline.Tests
         }
 
         [Fact]
-        public void LeilaoSemLances()
+        public void RetornaZeroDadoLeilaoSemLances()
         {
             // Arrange
             var leilao = new Leilao("Van Gogh");

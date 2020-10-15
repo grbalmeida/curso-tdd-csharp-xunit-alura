7d4dd492ee31022295124e24fb9527954698f9f2
gilvan <g.r.almeida@live.com>
2020-10-15 15:24:51 -0300

    Outra forma de avaliar o leilão

--- Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -7,6 +7,41 @@ namespace Alura.LeilaoOnline.Tests
     public class LeilaoTerminaPregao
     {
         [Theory]
+        [InlineData(1200, 1250, new double[] { 800, 1150, 1400, 1250 })]
+        public void RetornaValorSuperiorMaisProximoDadoLeilaoNessaModalidade(
+            double valorDestino, double valorEsperado, double[] ofertas)
+        {
+            // Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.IniciaPregao();
+
+            for (var i = 0; i < ofertas.Length; i++)
+            {
+                var valor = ofertas[i];
+
+                if (i % 2 == 0)
+                {
+                    leilao.RecebeLance(fulano, valor);
+                }
+                else
+                {
+                    leilao.RecebeLance(maria, valor);
+                }
+            }
+
+            // Act
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Theory]
         [InlineData(1200, new double[] { 800, 900, 1000, 1200 })]
         [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
         [InlineData(800, new double[] { 800 })]

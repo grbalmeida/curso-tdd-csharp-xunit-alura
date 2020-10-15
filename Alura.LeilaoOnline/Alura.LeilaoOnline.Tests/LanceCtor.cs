8ff9e5b4373c16ec65cc55c9039e36f998154815
gilvan <g.r.almeida@live.com>
2020-10-15 15:13:07 -0300

    Testando outra exceção

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LanceCtor.cs
@@ -0,0 +1,26 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LanceCtor
+    {
+        [Fact]
+        public void LancaArgumentExceptionDadoValorNegativo()
+        {
+            // Arrange
+            var valorNegativo = -100;
+
+            // Assert
+            var excecaoObtida = Assert.Throws<ArgumentException>(
+                // Act
+                () => new Lance(null, valorNegativo)
+            );
+
+            var msgEsperada = "Valor do lance deve igual ou maior que zero.";
+
+            Assert.Equal(msgEsperada, excecaoObtida.Message);
+        }
+    }
+}
